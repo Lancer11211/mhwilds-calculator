3b6a1ca53060e9e56504620127f5ae0334ba91c0
Lancer11211 <lancer1121@proton.me>
2026-01-22 01:26:16 +0100

    synchronized with remote configuration

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1042,13 +1042,6 @@ export const SeriesSkills: Record<SkillName, SeriesSkill> = {
       },
     },
   },
-  "Gogmapocalypse": {
-    uptime: true,
-    levels: {
-      2: { name: "Mutual Hostility I", element: 20, elementMul: 1.2, },
-      4: { name: "Mutual Hostility II", element: 40, elementMul: 1.3, },
-    },
-  },
   "Seregios's Tenacity": {
     uptime: true,
     levels: {
